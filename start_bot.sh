9cb30aad835e343a74bebb3073500ce53dc2ef1a
Александр Здункевич <ipfast@MacBook-Air-Aleksandr.local>
2025-11-29 17:29:08 +0100

    Add start_bot.sh wrapper script

---
+++ start_bot.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Wrapper script to start the bot with virtual environment
+
+# Change to bot directory
+cd /home/Fastkiller333/polish_bot
+
+# Activate virtual environment
+source venv/bin/activate
+
+# Run the bot
+python3 main.py
+
