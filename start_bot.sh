f9fb97810aa35754167e7a9cc2a9ac8319f20bac
Александр Здункевич <ipfast@MacBook-Air-Aleksandr.local>
2025-11-29 17:29:42 +0100

    Make start_bot.sh universal with dynamic path

--- start_bot.sh
+++ start_bot.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # Wrapper script to start the bot with virtual environment
 
+# Get the directory where this script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Change to bot directory
-cd /home/Fastkiller333/polish_bot
+cd "$SCRIPT_DIR"
 
 # Activate virtual environment
 source venv/bin/activate
